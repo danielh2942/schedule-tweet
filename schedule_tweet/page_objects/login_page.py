698b83a64c9c940706d157da539957bb7fb7e0fc
Daniel Hannon <12559951+danielh2942@users.noreply.github.com>
2020-05-04 14:55:20 +0100

    Update login_page.py

--- schedule_tweet/page_objects/login_page.py
+++ schedule_tweet/page_objects/login_page.py
@@ -1,6 +1,6 @@
-form = 'form.signin'
-form_username = 'form.signin [name="session[username_or_email]"]'
-form_password = 'form.signin [name="session[password]"]'
+form = 'form.r-13qz1uu'
+form_username = 'form.r-13qz1uu [name="session[username_or_email]"]'
+form_password = 'form.r-13qz1uu [name="session[password]"]'
 form_phone = '#challenge_response'
 
 
